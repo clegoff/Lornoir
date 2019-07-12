c645862aa413b0a2d586b749fa6a11f075f7ecd2
clegoff <c.legoffpro@gmail.com>
2019-07-12 09:54:00 +0200

    Merge #1

---
+++ app/controllers/layouts_controller.rb
@@ -0,0 +1,4 @@
+class LayoutsController < ApplicationController
+  def pierres
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
     root 'posts#index'
 
     get 'posts/index'
+    get 'home' => 'posts#index'
+    get 'about' => 'layouts#about'
+    get 'login' => 'accts#login'
+    get 'register' => 'layouts#register'
+    get 'pannier' => 'layouts#pannier'
+    get 'gemmes' => 'layouts#gemmes'
+    get 'pierres' => 'layouts#pierres'
 
-    resources :posts, :accts
+    resources :posts
 end
